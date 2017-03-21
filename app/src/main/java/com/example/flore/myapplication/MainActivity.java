f66ab6f35e9c67eb6dc6ea8a3ccb0649e2f3798c
Mario <mario.r.florea@gmail.com>
2017-03-21 21:16:41 +0200

    integrated modules

--- app/src/main/java/com/example/flore/myapplication/MainActivity.java
+++ app/src/main/java/com/example/flore/myapplication/MainActivity.java
@@ -1,69 +1,60 @@
 package com.example.flore.myapplication;
 
-import android.os.Environment;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import android.app.Activity;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import org.jtransforms.fft.DoubleFFT_1D;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Receiver myReceiver = new Receiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setButtonHandlers();
-        enableButtons(false);
+        enableButtons();
     }
 
     private void setButtonHandlers() {
-        ((Button) findViewById(R.id.btnStart)).setOnClickListener(btnClick);
-        ((Button) findViewById(R.id.btnStop)).setOnClickListener(btnClick);
+        ((Button) findViewById(R.id.btnConSender)).setOnClickListener(btnClick);
+        ((Button) findViewById(R.id.btnConReceiver)).setOnClickListener(btnClick);
+
     }
 
     private void enableButton(int id, boolean isEnable) {
         ((Button) findViewById(id)).setEnabled(isEnable);
     }
 
-    private void enableButtons(boolean isRecording) {
-        enableButton(R.id.btnStart, !isRecording);
-        enableButton(R.id.btnStop, isRecording);
+    private void enableButtons() {
+        enableButton(R.id.btnConSender, true);
+        enableButton(R.id.btnConReceiver, true);
+
     }
 
 
     private View.OnClickListener btnClick = new View.OnClickListener() {
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.btnStart: {
-                    enableButtons(true);
-                    myReceiver.startRecording();
+
+                case R.id.btnConReceiver: {
+                    enableButtons();
+                    Intent intent = new Intent(MainActivity.this, ReceiverActivity.class);
+                    startActivity(intent);
                     break;
                 }
-                case R.id.btnStop: {
-                    enableButtons(false);
-                    myReceiver.stopRecording();
+                case R.id.btnConSender: {
+                    enableButtons();
+                    Intent intent = new Intent(MainActivity.this, SenderActivity.class);
+                    startActivity(intent);
                     break;
                 }
             }
         }
     };
-
 }
+
 
